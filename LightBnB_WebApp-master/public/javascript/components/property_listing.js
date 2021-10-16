d61f9b968278f7903519eb59438cf120bab6d461
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-10-16 16:33:43 +0000

    Add ternary operator in footer for update and delete reservation buttons

--- LightBnB_WebApp-master/public/javascript/components/property_listing.js
+++ LightBnB_WebApp-master/public/javascript/components/property_listing.js
@@ -2,6 +2,8 @@ $(() => {
   window.propertyListing = {};
   
   function createListing(property, isReservation) {
+    console.log(property);
+
     return `
     <article class="property-listing">
         <section class="property-listing__preview-image">
@@ -20,6 +22,11 @@ $(() => {
           <footer class="property-listing__footer">
             <div class="property-listing__rating">${Math.round(property.average_rating * 100) / 100}/5 stars</div>
             <div class="property-listing__price">$${property.cost_per_night/100.0}/night</div>
+            ${isReservation.upcoming ? 
+              `<button id="update-property-${property.id}" class="update-button">Update</button>
+                <button id="delete-property-${property.id}" class="delete-button">Delete</button>
+              ` : ``
+            }
           </footer>
         </section>
       </article>
